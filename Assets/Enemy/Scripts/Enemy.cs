52860031fa0b879228797bf05bc6d55c20675417
Nicolas <nicolas.migues97@gmail.com>
2025-09-12 11:57:29 -0300

    ImplentShaderinDie

--- Assets/Enemy/Scripts/Enemy.cs
+++ Assets/Enemy/Scripts/Enemy.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetBool("IsDead", true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
