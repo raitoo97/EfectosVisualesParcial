e35ab9360babcfb970fe24e56ff3f3c9052bce7d
Nicolas <nicolas.migues97@gmail.com>
2025-09-13 15:23:24 -0300

    PreCode

---
+++ Assets/EscenaryManager.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+public class EscenaryManager : MonoBehaviour
+{
+    public List <Material> materials;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
