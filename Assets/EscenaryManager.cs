485ccf6487e7d5932b0bc91dc77c92bc4641b9bb
Nicolas <nicolas.migues97@gmail.com>
2025-09-19 00:32:24 -0300

    OKWall

--- Assets/EscenaryManager.cs
+++ Assets/EscenaryManager.cs
@@ -1,21 +1,5 @@
 using UnityEngine;
 public class EscenaryManager : MonoBehaviour
 {
-    private WallAnimation _wallAnimation;
-    private void OnEnable()
-    {
-        _wallAnimation = new WallAnimation(this);
-    }
-    void Start()
-    {
-        _wallAnimation.Onstart();
-    }
-    private void Update()
-    {
-        _wallAnimation.OnUpdate();
-    }
-    private void OnDisable()
-    {
-        _wallAnimation = null;
-    }
+
 }
