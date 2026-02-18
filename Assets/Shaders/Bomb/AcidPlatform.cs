28979e08c20a175ef9b225e77762f9f5ca261a7d
Nico <nicolas.migues97@gmail.com>
2026-02-18 07:13:39 -0300

    OKACIDBOMB

--- Assets/Shaders/Bomb/AcidPlatform.cs
+++ Assets/Shaders/Bomb/AcidPlatform.cs
@@ -1,18 +1,32 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class AcidPlatform : MonoBehaviour
 {
-
+    private Vector3 _targetSize;
+    private Vector3 _initSize;
+    private Coroutine _sizeCoroutine;
+    private void Awake()
+    {
+        _initSize = Vector3.zero;
+        transform.localScale = _initSize;
+        _targetSize = new Vector3(9f, 2f, 8f);
+    }
     void Start()
     {
-        
+        if(_sizeCoroutine == null)
+            _sizeCoroutine = StartCoroutine(SizeAnimation());
     }
-
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SizeAnimation()
     {
-        
+        float time = 0;
+        float duration = 0.5f;
+        while (time < duration)
+        {
+            transform.localScale = Vector3.Lerp(_initSize, _targetSize, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.localScale = _targetSize;
+        _sizeCoroutine = null;
     }
 }
