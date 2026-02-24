e58dbbdafab7e09ff941b73592b99438d3ec3995
Nico <nicolas.migues97@gmail.com>
2026-02-24 12:13:54 -0300

    OKWATER

--- Assets/Particles/AcidRain/SubEmitterSound.cs
+++ Assets/Particles/AcidRain/SubEmitterSound.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 public class SubEmitterSound : MonoBehaviour
 {
     private AudioSource _audio;
+    public Vector2 pitchRange = new Vector2(0.9f, 1.1f);
+    public Vector2 volumeRange = new Vector2(0.4f, 0.6f);
     void Awake()
     {
         _audio = GetComponent<AudioSource>();
     }
     void OnParticleCollision(GameObject other)
     {
-        Debug.Log("Playing sound");
-        _audio.PlayOneShot(_audio.clip);
+        _audio.pitch = Random.Range(pitchRange.x, pitchRange.y);
+        _audio.volume = Random.Range(volumeRange.x, volumeRange.y);
+        _audio.PlayOneShot(_audio.clip, 1.1f);
     }
 }
