95ad7491c03a3e12acca5dff671338c62de4e3c8
Nico <nicolas.migues97@gmail.com>
2026-02-25 08:15:01 -0300

    PREULTIMTATECINEMATIC

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -45,6 +45,7 @@ public class GameManager : MonoBehaviour
         {
             CinematicDirector.instance.ActivateCleanStair();
         }
+        SoundManager.Instance?.PlayClip(SoundManager.Instance.GetAudioClip("Alarm"), 0.05f, true);
     }
     public void StartTimer()
     {
