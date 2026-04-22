a6f2687221874fb8b6b40f99f5b5e49cf39f1161
Nico <nicolas.migues97@gmail.com>
2026-04-21 22:08:05 -0300

    OKPRIEMERAITERACION

--- Assets/Camera/PlayerCameraMovement.cs
+++ Assets/Camera/PlayerCameraMovement.cs
@@ -34,4 +34,24 @@ public class PlayerCameraMovement : CinemachineExtension
             }
         }
     }
+    public void SlerpToDirection(Vector3 targetDirection, float speed)
+    {
+        Quaternion currentRotation = Quaternion.Euler(-_startingRotation.y, _startingRotation.x, 0f);
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion smoothRotation = Quaternion.Slerp(currentRotation,targetRotation,speed * Time.deltaTime);
+        Vector3 euler = smoothRotation.eulerAngles;
+        _startingRotation.x = euler.y;
+        _startingRotation.y = -NormalizeAngle(euler.x);
+        _startingRotation.y = Mathf.Clamp(_startingRotation.y, -ClampleAngle, ClampleAngle);
+    }
+    private float NormalizeAngle(float angle)
+    {
+        if (angle > 180f)
+            angle -= 360f;
+        return angle;
+    }
+    public Vector3 GetCurrentForward()
+    {
+        return Quaternion.Euler(-_startingRotation.y, _startingRotation.x, 0f) * Vector3.forward;
+    }
 }
